dcdc59f8d9a085de34727326144c0e0348da0a1a
Evgeniy Gireev <145438140+eigireev@users.noreply.github.com>
2025-07-07 12:23:53 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+
+def click(key):
+    if key == "=":
+        try:
+            result = eval(entry.get())
+            entry.delete(0, tk.END)
+            entry.insert(tk.END, str(result))
+        except:
+            entry.delete(0, tk.END)
+            entry.insert(tk.END, "Ошибка")
+    elif key == "C":
+        entry.delete(0, tk.END)
+    else:
+        entry.insert(tk.END, key)
+
+root = tk.Tk()
+root.title("Калькулятор")
+
+entry = tk.Entry(root, width=30, borderwidth=5)
+entry.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+
+buttons = [
+    '7', '8', '9', '/',
+    '4', '5', '6', '*',
+    '1', '2', '3', '-',
+    '0', 'C', '=', '+'
+]
+
+row_val = 1
+col_val = 0
+
+for button in buttons:
+    tk.Button(root, text=button, padx=20, pady=10,
+              command=lambda key=button: click(key)).grid(row=row_val, column=col_val)
+    col_val += 1
+    if col_val > 3:
+        col_val = 0
+        row_val += 1
+
+root.mainloop()
